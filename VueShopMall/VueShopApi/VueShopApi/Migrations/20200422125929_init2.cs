d3b014ec4417a0756b5847aa4f77aa7592d73ad3
chriskowk <guoshaoyue@hotmail.com>
2020-04-25 15:46:32 +0800

    add vueshopmall

---
+++ VueShopMall/VueShopApi/VueShopApi/Migrations/20200422125929_init2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VueShopApi.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Size",
+                schema: "ps",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sizes",
+                schema: "ps",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sizes",
+                schema: "ps",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                schema: "ps",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ VueShopMall/VueShopApi/VueShopApi/Migrations/20200425023430_addmig20200425.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VueShopApi.Migrations
+{
+    public partial class addmig20200425 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
